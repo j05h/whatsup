57b76721a8e1e3ec4d42af7ff62428d1f3359b43
Josh Kleinpeter <josh@kleinpeter.org>
2014-07-26 14:36:37 -0500

    Clean up migration script

--- cassandra.ddl.py
+++ cassandra.ddl.py
@@ -2,57 +2,66 @@
 import sys
 import cassandra
 from cassandra.cluster import Cluster
-from cassandra.query import dict_factory
 
 CLUSTER_NODES = ['192.168.1.114']
 KEYSPACE = 'status'
 
-print "connecting to cluster %s" % CLUSTER_NODES
 cluster = Cluster(CLUSTER_NODES)
-session = cluster.connect()
 
-print "creating keyspace %s" % KEYSPACE
-try:
-  session.execute(
+def get_session():
+  return cluster.connect(KEYSPACE)
+
+def create_keyspace():
+
+  print "creating keyspace %s" % KEYSPACE
+  try:
+    cluster.connect().execute(
+        """
+        CREATE KEYSPACE %s
+        WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+        """ % KEYSPACE
+    )
+  except cassandra.AlreadyExists as e:
+    print "    %s" % e.message
+
+
+def create_tables():
+  print "creating tables in %s" % KEYSPACE
+  try:
+
+    get_session().execute(
       """
-      CREATE KEYSPACE %s
-      WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-      """ % KEYSPACE
-  )
-except cassandra.AlreadyExists as e:
-  print "    %s" % e.message
+      CREATE TABLE stats (
+        id uuid,
+        created_at timestamp,
+        site text,
+        service text,
+        message text,
+        description text,
+        state int,
+        PRIMARY KEY (id)
+      );
+      """
+    )
+  except cassandra.AlreadyExists as e:
+    print "    %s" % e.message
 
-session = cluster.connect('status')
+def create_indicies():
+  create_index("created_at")
+  create_index("site")
+  create_index("service")
 
-print "creating tables in %s" % KEYSPACE
-try:
 
-  session.execute(
-    """
-    CREATE TABLE stats (
-      id uuid,
-      created_at timestamp,
-      site text,
-      service text,
-      message text,
-      description text,
-      state int,
-      PRIMARY KEY (id)
-    );
-    """
-  )
-except cassandra.AlreadyExists as e:
-  print "    %s" % e.message
-
-def create_index(session, name):
+def create_index(name):
   print "creating index %s_key" % name
   try:
-    session.execute("CREATE INDEX %s_key on stats (%s);" % (name, name))
+    get_session().execute("CREATE INDEX %s_key on stats (%s);" % (name, name))
   except cassandra.InvalidRequest as e:
     print "    %s" % e.message
 
-create_index(session, "created_at")
-create_index(session, "site")
-create_index(session, "service")
 
+print "initializing cluster %s" % CLUSTER_NODES
+create_keyspace()
+create_tables()
+create_indicies()
 print "done."
