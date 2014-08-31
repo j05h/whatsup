22f1088f858cf9b5513ffe5a1a0f3fd20b344db2
cephaslr <cephaslr@yahoo.com>
2014-08-30 19:41:53 -0700

    Updating simulate script

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -6,19 +6,21 @@ Seeds the database with dummy data
 
 import random
 import subprocess
+import requests
+import json
 
 sites = ['US-TX', 'US-NC', 'EU-AERO', 'AUS-MEL']
 services = ['nova', 'glance', 'neutron', 'cinder', 'swift']
 messages = {'nominal': 1, 'reduced capabilities': 0, 'failed': -1}
+url = 'http://localhost:5000/api/v1.0/status'
+headers = {'content-type': 'application/json'}
+auth = ('admin', 'testing')
 
 for site in sites:
     for service in services:
         message = messages.keys()[random.randrange(0, 3)]
-
-        syscmd = "curl -u admin:testing -H \"Content-Type: application/json\"\
--X POST -d \'{\"site\":\"" + site + "\",\"service\":\"" + service + "\",\
-\"message\":\"" + message + "\",\"state\":%d}\'" % messages[message] + " \
-localhost:5000/api/v1.0/status"
-        print syscmd
-        subprocess.Popen(syscmd, stdout=subprocess.PIPE,
-                         shell=True).communicate()[0]
+        payload = {'site': site, 'service': service, 'message': message,
+                   'state': messages[message]}
+        r = requests.post(url, data=json.dumps(payload), headers=headers,
+                          auth=auth)
+        print(r.text)
